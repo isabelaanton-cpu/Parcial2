7ef444d050dfab314168dddd4529aa412943c984
isabelaanton-cpu <isabela.anton@escola.pr.gov.br>
2026-04-06 07:48:04 -0300

    desafio5.py

---
+++ desafio5.py
@@ -0,0 +1,8 @@
+# Solicita a medida da base do triângulo
+base = float(input("Digite o valor da base do triângulo: ")) 
+# Solicita a medida da altura do triângulo
+altura = float(input("Digite o valor da altura do triângulo: ")) 
+# Aplica a fórmula da área: (base * altura) / 2
+area = (base * altura) / 2 
+# Exibe o valor da área calculado
+print(f"A área do triângulo é: {area}")
